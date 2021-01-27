ca56f23b7ea2f2a6a1ded1058328dd562dae3b08
Miguel Moreo <miguelangel.moreo@gmail.com>
2021-01-27 18:40:35 +0100

    Fist test: 3P, 3E, perfect Match

---
+++ src/main/java/com/smarthost/roommanager/model/RoomManager.java
@@ -0,0 +1,84 @@
+package com.smarthost.roommanager.model;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class RoomManager {
+
+	private List<Integer> customers;
+	
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	 class Occupancy {
+		
+		private int usagePremium;
+		private int usageEconomy;
+		private int pricePremium;
+		private int priceEconomy;
+	}
+	
+	
+	
+	
+	public Occupancy calculateOccupancy(int premium, int economy) {
+		
+		List<Integer> usePremium = customers
+				.stream()
+				.filter(p -> p >= 100)
+				.sorted(Comparator.reverseOrder())
+				.limit(premium)
+				.collect(Collectors.toList());
+		
+		
+		List<Integer> useEconomy = customers
+				.stream()
+				.filter(p -> p < 100)
+				.sorted(Comparator.reverseOrder())
+				.limit(economy)
+				.collect(Collectors.toList());
+		
+		/*IntStream
+			    .iterate( premium - 1,  n -> n - 1)
+			    .takeWhile(n -> n > 0)
+			    .mapToObj(pene::get)
+			    .forEach(result::add);
+			*/    
+		
+		if (usePremium.size() < premium) {
+			// premium free
+			int premiumFree = premium - usePremium.size();
+			// rest of cheap
+			 List<Integer> restOfCheap = customers
+				.stream()
+				.filter(p -> p < 100)
+				.sorted(Comparator.reverseOrder())
+				//.limit(economy)
+				.collect(Collectors.toList());
+			
+			restOfCheap.removeIf(x -> useEconomy.contains(x));
+			
+		}
+		
+		int pricePremium = usePremium.stream().mapToInt(Integer::valueOf).sum();
+		int priceEconomy = useEconomy.stream().mapToInt(Integer::valueOf).sum();
+		
+		Occupancy occupancy = new Occupancy();
+		occupancy.setUsagePremium(usePremium.size());
+		occupancy.setUsageEconomy(useEconomy.size());
+		occupancy.setPricePremium(pricePremium);
+		occupancy.setPriceEconomy(priceEconomy);
+		
+		return occupancy;
+	}
+	
+	public void addCustomers(List<Integer> customers) {
+		this.customers = customers;
+	}
+}
---
+++ src/test/java/com/smarthost/roommanager/model/RoomManagerTest.java
@@ -0,0 +1,38 @@
+package com.smarthost.roommanager.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.smarthost.roommanager.model.RoomManager.Occupancy;
+
+class RoomManagerTest {
+
+	private RoomManager manager;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		manager = new RoomManager();
+	}
+
+	@Test
+	void calculateOccupancyPerfectMatch() {
+		List<Integer> customers = Arrays.asList( new Integer[] {23, 45, 155, 374, 22, 99, 100, 101, 115, 209} );
+		
+		int premium = 3;
+		int economy = 3;
+		manager.setCustomers(customers);
+		
+		Occupancy occupancy = manager.calculateOccupancy(premium, economy);
+		assertEquals(3, occupancy.getUsagePremium());
+		assertEquals(3, occupancy.getUsageEconomy());
+		assertEquals(738, occupancy.getPricePremium());
+		assertEquals(167, occupancy.getPriceEconomy());
+	
+	}
+
+}
