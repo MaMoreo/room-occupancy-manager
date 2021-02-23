8879418c82a8269c83b19bc2d5616882b100b190
Miguel Moreo <miguelangel.moreo@gmail.com>
2021-02-23 16:23:06 +0100

    Remove custom exception

--- src/main/java/com/smarthost/roommanager/config/CustomerDeserializer.java
+++ src/main/java/com/smarthost/roommanager/config/CustomerDeserializer.java
@@ -2,9 +2,7 @@ package com.smarthost.roommanager.config;
 
 import java.io.IOException;
 
-import javax.money.CurrencyUnit;
 import javax.money.Monetary;
-import javax.money.UnknownCurrencyException;
 
 import org.javamoney.moneta.FastMoney;
 
@@ -12,7 +10,6 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.smarthost.roommanager.exception.BadRequestException;
 import com.smarthost.roommanager.model.Customer;
 
 public class CustomerDeserializer extends StdDeserializer<Customer> {
@@ -30,12 +27,6 @@ public class CustomerDeserializer extends StdDeserializer<Customer> {
 		JsonNode node = jp.getCodec().readTree(jp);
 		String currency = node.get("amount").get("currency").asText();
 		Number number = node.get("amount").get("number").numberValue();
-		CurrencyUnit currencyUnit;
-		try {
-			currencyUnit = Monetary.getCurrency(currency);
-		} catch (UnknownCurrencyException e) {
-			throw new BadRequestException(e.getMessage());
-		}
-		return new Customer(FastMoney.of(number, currencyUnit));
+		return new Customer(FastMoney.of(number, Monetary.getCurrency(currency)));
 	}
 }
